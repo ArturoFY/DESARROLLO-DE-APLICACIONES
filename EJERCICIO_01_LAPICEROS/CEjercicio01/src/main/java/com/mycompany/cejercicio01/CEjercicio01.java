0fc14192c853fbc8757a48ec3c307ca8fd96bd17
Diego Flores Yupanqui <diegoafy@gmail.com>
2025-04-12 23:21:52 -0500

    Add files via upload

---
+++ EJERCICIO_01_LAPICEROS/CEjercicio01/src/main/java/com/mycompany/cejercicio01/CEjercicio01.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.cejercicio01;
+
+import Vista.Lapiceros;
+
+public class CEjercicio01 {
+
+    public static void main(String[] args) {
+        new Lapiceros().setVisible(true);
+    }
+}

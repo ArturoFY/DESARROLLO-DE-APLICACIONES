fe1f8ffbf5d102004733d14d73c31db8440e069d
Diego Flores Yupanqui <diegoafy@gmail.com>
2025-04-13 20:22:01 -0500

    Add files via upload

---
+++ S1_EJERCICIO_01_CAMISAS/CEjercicio_Semana01/src/main/java/com/mycompany/cejercicio_semana01/CEjercicio_Semana01.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.cejercicio_semana01;
+
+import Vista.VentaCamisas;
+
+public class CEjercicio_Semana01 {
+
+    public static void main(String[] args) {
+        new VentaCamisas().setVisible(true);
+    }
+}

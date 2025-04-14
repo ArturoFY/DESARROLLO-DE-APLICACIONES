d94ba01b9bcbdeb8d56fedc26ea9f208faf10a12
Diego Flores Yupanqui <diegoafy@gmail.com>
2025-04-13 20:22:34 -0500

    Add files via upload

---
+++ S1_EJERCICIO_02_FERIA/CEjercicio02_Semana01/src/main/java/com/mycompany/cejercicio02_semana01/CEjercicio02_Semana01.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cejercicio02_semana01;
+
+import Vista.InversionFeria;
+
+/**
+ *
+ * @author diego
+ */
+public class CEjercicio02_Semana01 {
+
+    public static void main(String[] args) {
+        new InversionFeria().setVisible(true);
+    }
+}
